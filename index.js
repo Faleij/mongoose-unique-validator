4d77b0b34a4f0b3fa7e55a168f813f1a0ed87df4
Faleij <faleij@live.com>
2014-12-11 16:11:44 +0100

    Fixed support for compound indexes

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-module.exports = function (schema, options) {
+module.exports = function(schema, options) {
     var message = 'Error, expected `{PATH}` to be unique. Value: `{VALUE}`';
     if (options && options.message) {
         message = options.message;
     }
 
     var indexes = schema.indexes(); // [ [{paths}, {options}], ...]
-    indexes = Object.keys(indexes).reduce(function (p, key) {
-        Object.keys(indexes[key][0]).forEach(function (path) {
-            p[path] = p[path] || {};
-            Object.keys(indexes[key][1]).forEach(function (optionKey) {
-                p[path][optionKey] = indexes[key][1][optionKey];
-            });
-        });
-        return p;
-    }, {}); // transform into { path: {options} }
-
-    schema.eachPath(function (path, schemaType) {
-        if (indexes[path].unique) {
-            var validator = buildUniqueValidator(path);
-            message = buildMessage(schemaType.options.unique || indexes[path].unique, message);
-            schemaType.validate(validator, message);
+    
+    indexes.forEach(function (indexPair) {
+        if (indexPair[1].unique) {
+            var validator = buildUniqueValidator(indexPair[0]);
+            var path = Object.keys(indexPair[0])[0];
+            schema.path(path).validate(validator, buildMessage(indexPair[1].unique, message)); // set validator on only first path
         }
     });
 };
 
-function buildMessage(uniqueOption, message){
+function buildMessage(uniqueOption, message) {
     return typeof uniqueOption === 'string' ? uniqueOption : message;
 }
 
-function buildUniqueValidator(path) {
-    return function (value, respond) {
+function buildUniqueValidator(paths) {
+    return function(value, respond) {
         var model = this.model(this.constructor.modelName);
-        var query = buildQuery(path, value, this._id);
+        var query = buildQuery.call(this, paths, value, this._id);
         var callback = buildValidationCallback(respond);
         model.findOne(query, callback);
     };
 }
 
-function buildQuery(field, value, id) {
-    var query = { $and: [] };
+function buildQuery(fields, value, id) {
+    var query = {
+        $and: []
+    };
     var target = {};
-    target[field] = value;
-    query.$and.push({ _id: { $ne: id } });
+    Object.keys(fields).forEach(function (field) {
+        target[field] = this.get(field);
+    }.bind(this));
+    query.$and.push({
+        _id: {
+            $ne: id
+        }
+    });
     query.$and.push(target);
     return query;
 }
 
 function buildValidationCallback(respond) {
-    return function (err, document) {
+    return function(err, document) {
         respond(!document);
     };
 }
